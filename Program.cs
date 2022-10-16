f9d5aed8ee0c996dfdb45d2ea8d0b51f8960d8f2
Ivan <PonomarevIK99@yandex.ru>
2022-10-16 22:21:25 +0500

    Simple CRUD operation examples

--- Program.cs
+++ Program.cs
@@ -4,6 +4,44 @@ internal class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        // C - Create
+        //using (ApplicationContext db = new ApplicationContext())
+        //{
+        //    User user1 = new User { Name = "Tom", Age = 33 };
+        //    User user2 = new User { Name = "Alice", Age = 26 };
+
+        //    db.Users.AddRange(user1, user2);
+        //    db.SaveChanges();
+        //}
+
+        // R - Read
+        //using (ApplicationContext db = new ApplicationContext())
+        //{
+        //    var users = db.Users.ToList();
+        //}
+
+        // U - Update
+        //using (ApplicationContext db = new ApplicationContext())
+        //{
+        //    User user = db.Users.FirstOrDefault();
+        //    if (user != null)
+        //    {
+        //        user.Name = "Bob";
+        //        user.Age = 44;
+        //        db.Users.Update(user);
+        //        db.SaveChanges();
+        //    }
+        //}
+
+        // D - Delete
+        //using (ApplicationContext db = new ApplicationContext())
+        //{
+        //    User user = db.Users.FirstOrDefault();
+        //    if (user != null)
+        //    {
+        //        db.Users.Remove(user);
+        //        db.SaveChanges();
+        //    }
+        //}
     }
 }
